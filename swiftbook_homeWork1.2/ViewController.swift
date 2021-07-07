e68b08055f9461fa4d0c5579850ce579200f2b45
Nikishin Aleksandr <ex.kick@gmail.com>
2021-07-08 02:01:39 +0300

    Initial Commit

---
+++ swiftbook_homeWork1.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swiftbook_homeWork1.2
+//
+//  Created by Aleksandr on 08.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
